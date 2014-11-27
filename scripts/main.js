98c2e6575a49347d75b31ce96891d63e43e115b8
fan li <fan.li@rea-group.com>
2014-11-27 17:35:03 +1100

    define a model.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-var Person = function(config){
-  this.name = config.name;
-  this.age = config.age;
-  this.occupation = config.occupation;
-};
-
-Person.prototype.work = function(){
-  return this.name + ' is working.';
-};
+var Person = Backbone.Model.extend({
+  defaults: {
+    name: 'Guest User',
+    age: 23,
+    occupation: 'Workser'
+  },
+  work: function () {
+    return this.get('name') + ' is working.';
+  }
+});
