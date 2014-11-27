860ce27e2122c6246b959857874746db3e6bba00
fan li <fan.li@rea-group.com>
2014-11-27 17:44:11 +1100

    add validate function to confirm age more than 0 and name has a value.

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,17 @@ var Person = Backbone.Model.extend({
     age: 23,
     occupation: 'Workser'
   },
+
+  validate: function (attributes) {
+    if ( attributes.age < 0 ) {
+	return 'Age must be positive.';
+    }
+
+    if ( attributes.name ) {
+	return 'Every person must have a name.';
+    }
+  },
+
   work: function () {
     return this.get('name') + ' is working.';
   }
