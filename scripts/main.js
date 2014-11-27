2094eb0f36b5f4332d56f5be90ed12ca7149e48d
fan li <fan.li@rea-group.com>
2014-11-27 20:44:54 +1100

    render the element and data on web page by using the low way.

--- scripts/main.js
+++ scripts/main.js
@@ -3,26 +3,28 @@ var Person = Backbone.Model.extend({
     name: 'Guest User',
     age: 23,
     occupation: 'Workser'
-  },
-
-  validate: function (attributes) {
-    if ( attributes.age < 0 ) {
-	return 'Age must be positive.';
-    }
-
-    if ( attributes.name ) {
-	return 'Every person must have a name.';
-    }
-  },
-
-  work: function () {
-    return this.get('name') + ' is working.';
   }
 });
 
-var sparrow = new Person({name: 'sparrow', age: 28, occupation: 'dev'});
-sparrow.on("invalid", function(model, error){
-  alert(error);
+var PersonView = Backbone.View.extend({
+  tagName: 'li',
+  className: 'person',
+  id: 'person-id',
+
+  template: _.template(" () - "),
+
+  initialize: function () {
+    this.render();
+  },
+
+  render: function() {
+    // anti-pattern
+    this.$el.html( this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('occupation') );
+  }
 });
 
-sparrow.save("age", -2);
+var sparrow = new Person({name: 'sparrow', age: '28', occupation: 'dev'});
+var personView = new PersonView({model: sparrow});
+
+console.log(personView.el);
+console.log(personView.$el);
